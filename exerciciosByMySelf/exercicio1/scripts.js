77772b1443b5a83bca9620c55f6c542df9d1cfe2
Arthur Menadel <menadelarthur@gmail.com>
2024-01-10 23:36:32 -0300

    em progresso

--- exerciciosByMySelf/exercicio1/scripts.js
+++ exerciciosByMySelf/exercicio1/scripts.js
@@ -1,75 +1,30 @@
-/*
-    Write a javaScript program to check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100.
+// DOM Manipulation
 
-*/ 
-
-const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a + b) === 100;
-
-console.log(isEqualTo100(100, 0));
-console.log(isEqualTo100(0, 100));
-
-console.log(isEqualTo100(10,0))
-console.log(isEqualTo100(0,10))
-
-console.log(isEqualTo100(20,80))
-console.log(isEqualTo100(50,50))
-
-/* ---------------------------------------------- */
-
-/*
-Write Java Script program to get the extension of a filename
-*/
+//GetElementById()
 
 
-const getFileExtension = (str) => str.slice(str.lastIndexOf ('.'));
+const title = document.getElementById('main-heading')
+console.log(title)
 
+/*//GetElementByClassName()
 
-console.log(getFileExtension('index.html'))
-console.log(getFileExtension('webpack.config.js'))
+const listItems = document.getElementsByClassName('list-items')
 
+console.log(listItem)
 
-/*
+//getElementsByTagName()
 
-Exercise 3 
+const listItem = document.getElementsByTagName('li')
+console.log(listItem)
 
-Write a JavaScript program to replace every character in a given string with the character following it in the alphabet
+//querySelector()
 
-*/ 
+const container = document.querySelector('div')
+console.log(container)
 
-const moveCharsForward = (str) => str
-                                  .split('')
-                                  .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
-                                  .join('')
-console.log(moveCharsForward('abcd'))
+//querySelectorAll()
 
-
-/*
-Exercise 4:
- write a JavaScript program to get the current date
- Expected Output:
- mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyyy, dd/mm/yyyy
+const container2 = document.querySelectorAll('div')
+console.log(container)
 
 */
-
-
-const formatDate = (date = new Date()) => {
-    const days = date.getDate() 
-    const months = date.getMonth() + 1
-    const years = date.getFullYear()
-    return ` ${days} / ${months} / ${years} `
-}
-
-
-console.log(formatDate())
-
-
-/*
-Exercicio 5
-
-Create a JavaScript program to create a new string adding "New!" in front of a given string in the given string begins with already then return the original string.
-*/
-
-const addNew =  (str) => 
-    str.indexOf("New!" ) === 0 ? str:  `New! ${str}`
-
-    console.log(addNew('Offers'))
